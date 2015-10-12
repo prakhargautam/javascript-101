49da6ec4a0a3c5006495262b8dd002f51838fdbd
Prakhar Gautam <prakhargautam96@gmail.com>
2015-10-12 19:13:03 +0530

    Update 3.js
    Corrected solution (valid for Large numbers)

--- 3.js
+++ 3.js
@@ -1,30 +1,12 @@
-var eratosthenes = function(n) {
-    // Eratosthenes algorithm to find all primes under n
-    var array = [], upperLimit = Math.sqrt(n), output = [];
-    var max;
-    // Make an array from 2 to (n - 1)
-    for (var i = 0; i < n; i++) {
-        array.push(true);
-    }
-
-    // Remove multiples of primes starting from 2, 3, 5,...
-    for (i = 2; i <= upperLimit; i++)
+var n=600851475143;
+var max=0;
+for(var i=2; i<=n; i++)
+{
+    if(n%i===0)
     {
-        if (array[i]) {
-            for (var j = i * i; j < n; j += i) {
-                array[j] = false;
-            }
-        }
+        n=n/i;
+        max=i;
+        console.log(i);
     }
-
-    // All array[i] set to true are primes
-    for (i = 2; i < n; i++) {
-        if((array[i])&&(n%i===0)) {
-            max=i;
-        }
-    }
-
-    return max;
-};
-
-eratosthenes(600851475143);
+}
+console.log(max);
