c47d1a713991fab3ab062a0b22f1221ed69f0b65
Prakhar Gautam <prakhargautam96@gmail.com>
2015-10-12 19:01:03 +0530

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,30 @@
+var eratosthenes = function(n) {
+    // Eratosthenes algorithm to find all primes under n
+    var array = [], upperLimit = Math.sqrt(n), output = [];
+    var max;
+    // Make an array from 2 to (n - 1)
+    for (var i = 0; i < n; i++) {
+        array.push(true);
+    }
+
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (i = 2; i <= upperLimit; i++)
+    {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+
+    // All array[i] set to true are primes
+    for (i = 2; i < n; i++) {
+        if((array[i])&&(n%i===0)) {
+            max=i;
+        }
+    }
+
+    return max;
+};
+
+eratosthenes(600851475143);
